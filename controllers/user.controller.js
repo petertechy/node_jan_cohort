582528c2f5b8829b864c77d93223fcfaa9f5e785
petertechy <petertechy01@gmail.com>
2025-02-03 14:06:04 +0100

    Nodemailer Email sending

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,15 +1,39 @@
-const express = require('express')
 const userModel = require('../models/users.model')
+const nodemailer = require('nodemailer')
 const createUser = (req, res) =>{
     let form = new userModel(req.body)
     form.save()
     .then(()=>{
         console.log("User Info saved successfully")
+
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: 'petertechy01@gmail.com',
+              pass: 'vzrk ilmj zvur wmme'
+            }
+          });
+          
+          var mailOptions = {
+            from: 'petertechy01@gmail.com',
+            to: [req.body.email, 'ihezuechigozie@gmail.com', 'adeyemipraise97@gmail.com',"ikolabaolanrewaju@gmail.com"],
+            subject: 'Welcome to Our Node Application',
+            html: '<h1>Congratulations!!!</h1><p>You are welcome to our Node Application</p>'
+          };
+          
+          transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+          });
+
+        res.redirect('/user/all-users')
     })
     .catch((err)=>{
         console.log(err, "User Info not saved")
     })
-    res.redirect('/user/all-users')
 }
 
 const fetchUsers = (req, res) =>{
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //dotenv, deployment, api testing, email sending
 
+//thunder client, postman, swagger, insomia
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
