c2458e14e1fef7b900ab20ce66d78f96ce2d29fb
petertechy <petertechy01@gmail.com>
2025-02-06 16:53:02 +0100

    Modify user controller logic

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,36 +4,38 @@ const createUser = (req, res) =>{
     let form = new userModel(req.body)
     form.save()
     .then(()=>{
-        console.log("User Info saved successfully")
+      console.log("User Info saved successfully")
+      console.log(req.body)
+      res.send({message: "Successfully registered", form})
 
-        var transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-              user: process.env.GMAIL_USER,
-              pass: process.env.GMAIL_PASS
-            }
-          });
+        // var transporter = nodemailer.createTransport({
+        //     service: 'gmail',
+        //     auth: {
+        //       user: process.env.GMAIL_USER,
+        //       pass: process.env.GMAIL_PASS
+        //     }
+        //   });
           
-          var mailOptions = {
-            from: 'petertechy01@gmail.com',
-            to: [req.body.email, 'ihezuechigozie@gmail.com', 'adeyemipraise97@gmail.com',"ikolabaolanrewaju@gmail.com"],
-            subject: 'Welcome to Our Node Application',
-            html: '<h1>Congratulations!!!</h1><p>You are welcome to our Node Application</p>'
-          };
+        //   var mailOptions = {
+        //     from: 'petertechy01@gmail.com',
+        //     to: [req.body.email, 'ihezuechigozie@gmail.com', 'adeyemipraise97@gmail.com',"ikolabaolanrewaju@gmail.com"],
+        //     subject: 'Welcome to Our Node Application',
+        //     html: '<h1>Congratulations!!!</h1><p>You are welcome to our Node Application</p>'
+        //   };
           
-          transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-              console.log(error);
-            } else {
-              console.log('Email sent: ' + info.response);
-            }
-          });
+        //   transporter.sendMail(mailOptions, function(error, info){
+        //     if (error) {
+        //       console.log(error);
+        //     } else {
+        //       console.log('Email sent: ' + info.response);
+        //     }
+        //   });
 
         // res.redirect('/user/all-users')
-        res.send({message: "Succefully registered", form})
     })
     .catch((err)=>{
         console.log(err, "User Info not saved")
+        res.status(500).send({ error: err.message });
     })
 }
 
@@ -41,15 +43,53 @@ const fetchUsers = (req, res) =>{
     userModel.find()
     .then((users)=>{
         console.log(users)
-        res.send({status: true, users})
-        // res.render("allUsers", {allUsers: users})
+        // res.send({status: true, users})
+        res.render("allUsers", {allUsers: users})
     })
+    .catch((err) => {
+      console.error("Error fetching users:", err);
+      res.status(500).send({ error: err.message });
+  });
 }
 
 const signInUser = (req, res) =>{
 
 }
 
+const deleteUser = async (req, res)=>{
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await userModel.findByIdAndDelete(id);
+    if (!deletedUser) return res.status(404).send("User not found");
+
+    res.redirect("/user/all-users");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+  
+}
+
+const updateUser = async (req, res)=>{
+  try {
+    const { id } = req.params;
+    const { firstName, lastName, email, age } = req.body;
+
+    const updatedUser = await userModel.findByIdAndUpdate(
+      id,
+      { firstName, lastName, email, age },
+      { new: true }
+    );
+
+    if (!updatedUser) return res.status(404).send("User not found");
+
+    res.redirect("/user/all-users");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+  
+}
 
 
-module.exports = {createUser, signInUser, fetchUsers}
+
+module.exports = {createUser, signInUser, fetchUsers, deleteUser, updateUser}
